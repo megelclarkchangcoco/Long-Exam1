49d35fb880e03590691e61ee4a8ef25e9e4d61cd
skyclarkchangcoco <123862183+megelclarkchangcoco@users.noreply.github.com>
2023-05-21 21:58:54 +0800

    Delete Main.java

--- Main.java
+++
@@ -1,9 +0,0 @@
-package Portfolio;
-
-public class Main {
-	// main method to create the frame
-    public static void main(String[] args) {
-
-    	new MyPortfolioFrame().setVisible(true);
-    }
-}
